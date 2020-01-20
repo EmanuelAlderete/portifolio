9232df83473a81b499d53d87a54978d245e082f0
Luis Emanuel Alderete <luisemanuel.alderete@gmail.com>
2020-01-20 04:38:46 -0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HomeSection, ResetCSS, FundationsSection, BounceArrow, StyledLink } from './styles';
+
+
+
+import cloudImg from './images/cloud.svg';
+import fastImg from './images/fast.svg';
+import graphImg from './images/graph.svg';
+import arrow from './images/white-arrow.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HomeSection>
+        <div>
+          <ResetCSS />
+          <h1>EMANUEL ALDERETE</h1>
+          <p>DESENVOLVEDOR WEB</p>
+        </div>
+        <BounceArrow to={"fundations"} spy={true} smooth={true} offset={50} delay={150} duration={1000}>
+          <img src={arrow}  alt="arrow-down"/>
+        </BounceArrow>
+      </HomeSection>
+      <FundationsSection name="fundations" >
+        <div className="main title">
+          <h1>Fundamentos</h1>
+          <p>
+            Ao desenvolver um sistema, meu objetivo é entregar um produto rápido, seguro e completo.
+            Você pode conferir um pouco sobre cada princípio que será implementado em seu projeto.
+                </p>
+          <div className="buttons">
+            <StyledLink to="techs">TECNOLOGIAS</StyledLink>
+            <StyledLink to="projects">PROJETOS</StyledLink>
+          </div>
+        </div>
+        <div className="main">
+          <div className="icons">
+            <img src={cloudImg} alt="" />
+            <div>
+              <h4>ARMAZENAMENTO EM NUVEM</h4>
+              <p>Acesso 24/7. Dados seguros com backups feitos automaticamente.</p>
+            </div>
+          </div>
+          <div className="icons">
+            <img src={fastImg} alt="" />
+            <div>
+              <h4>VELOCIDADE E FLUIDEZ</h4>
+              <p>
+                Aplicações velozes que funcionam em qualquer dispositivo.
+                        </p>
+            </div>
+          </div>
+          <div className="icons">
+            <img src={graphImg} alt="" />
+            <div>
+              <h4>ESCALABILIDADE</h4>
+              <p>
+                Seu negócio aumentou? Sem problema, seu sistema aumenta junto.
+                        </p>
+            </div>
+          </div>
+        </div>
+      </FundationsSection>
     </div>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,145 @@
+import styled, { createGlobalStyle, keyframes } from 'styled-components';
+import { flipInY, bounce } from 'react-animations';
+import { Link } from "react-scroll";
+
+const flixInXAnimation = keyframes`${flipInY}`;
+const bounceAnimation = keyframes`${bounce}`;
+
+export const ResetCSS = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap');
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+        font-family: 'Roboto Mono', monospace;
+    }
+`;
+
+export const HomeSection = styled.section`
+    height: 100vh;
+    background: linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7) ), url("https://images.unsplash.com/photo-1484807352052-23338990c6c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80");
+    background-size: cover;
+    color: #FFF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 10%;
+    flex-wrap: wrap;
+
+    div {
+        min-width: 100%;
+        display: inline;
+        
+    }
+
+    h1 {
+        letter-spacing: 2px;
+        font-weight: lighter;
+        font-size: 2em;
+        display: block;
+    }
+
+    p {
+        font-size: 1.5em;
+        font-weight: lighter;
+        display: block;
+    }
+`;
+
+export const FundationsSection = styled.section`
+    height: 95vh;
+    max-width: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: row-reverse wrap;
+    align-items: center;
+
+    div.main { 
+        flex: 1 1 50%; 
+        padding: 20px; 
+    }
+
+    div.icons { 
+        display: flex; 
+        align-items: center; 
+        margin: 10px 0; 
+    }
+
+    div.icons div {
+        margin-left: 10px;
+    }
+
+    div.icons  p {
+        font-size: .8em;
+    }
+
+    img { 
+        width: 100px; 
+    }
+
+    div.icons:hover > img { 
+        animation: 1.3s ${flixInXAnimation}; 
+    }
+
+    h1 { 
+        font-size: 3em; 
+    }
+
+    h4 {
+        font-weight: bold;
+    }
+
+    div.buttons {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    @media (min-width: 300px){
+        div.main {
+            min-width: 300px;
+        }
+    }
+
+    @media (max-width: 300px) {
+        div.main {
+            min-width: 200px;
+        }
+    }
+`;
+
+export const StyledLink = styled(Link)`
+    box-sizing: border-box;
+    text-decoration: none;
+    background: #000;
+    color: #FFF;
+    width:50%;
+    font-size: 20px;
+    padding: 10px;
+    display: inline-block;
+    margin-top: 15px;
+    text-align: center;
+    transition: all .3s;
+    position: relative;
+
+    :hover {
+        box-sizing: border-box;
+        padding: 9px;
+        background: #FFF;
+        border: 1px solid #000;
+        color: #000;
+    }
+`;
+
+export const BounceArrow = styled(Link)`
+    animation: 2s ${bounceAnimation};
+    animation-iteration-count:infinite;
+    cursor: pointer;
+    bottom: 0;
+    position: absolute;
+
+    img {
+        height: 50px;
+    }
+`;
